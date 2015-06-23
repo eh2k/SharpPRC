d4ab9c29be76579c82cc545c39f05a5eae8fc10d
eheidt <taibal@gmx.net>
2015-06-23 21:58:55 +0200

    2

--- SharpPRC.cs
+++ SharpPRC.cs
@@ -38,27 +38,27 @@ namespace SharpPRC
                 new DeflateStream(stream, CompressionLevel.Optimal));
         }
 
-        public void Write(string value)
+        public void WriteString(string value)
         {
             _streamWriter.Write(value);
         }
 
-        public void Write(bool value)
+        public void WriteBool(bool value)
         {
             _streamWriter.Write(value);
         }
 
-        public void Write(double value)
+        public void WriteDouble(double value)
         {
             _streamWriter.Write(value);
         }
 
-        public void Write(UInt32 value)
+        public void WriteUint32(UInt32 value)
         {
             _streamWriter.Write(value);
         }
 
-        public void Write(List<UInt32> list)
+        public void WriteUInt32List(List<UInt32> list)
         {
             UInt32 size = (UInt32)list.Count;
             _streamWriter.Write(size);
@@ -66,7 +66,7 @@ namespace SharpPRC
                 _streamWriter.Write(value);
         }
 
-        public void Write(List<double> list)
+        public void WriteDoubleList(List<double> list)
         {
             UInt32 size = (UInt32)list.Count;
             _streamWriter.Write(size);
@@ -74,12 +74,192 @@ namespace SharpPRC
                 _streamWriter.Write(value);
         }
 
-        public void Write(List<PRCTessFace> list)
+        internal void WriteUncompressedBlock(string p1, int p2)
         {
-            UInt32 size = (UInt32)list.Count;
-            _streamWriter.Write(size);
-            foreach (var value in list)
-                value.SerializeContentTessFace(this);
+            throw new NotImplementedException();
+        }
+
+        internal void WriteUncompressedUnsignedInteger(uint minimal_version_for_read)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void SerializeFileStructureUncompressedUniqueId(PRCUniqueId file_structure_uuid)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SerializeContentBaseTessData(PRCContentBaseTessData t)
+        {
+            WriteBool(t.is_calculated);
+            WriteDoubleList(t.coordinates);
+        }
+
+        private void SerializeContent3DTess(PRC3DTess t)
+        {
+            SerializeContentBaseTessData(t);
+
+            WriteBool(t.has_faces);
+            WriteBool(t.has_loops);
+            WriteBool(false); //must recalculate normals
+            WriteDoubleList(t.normal_coordinate);
+            WriteUInt32List(t.wire_index);
+            WriteUInt32List(t.triangulated_index);
+
+            WriteUint32((UInt32)t.face_tessellation.Count);
+            foreach (var face in t.face_tessellation)
+                SerializeContentTessFace(face);
+
+            WriteDoubleList(t.texture_coordinate);
+        }
+
+        public void Serialize3DTess(PRC3DTess t)
+        {
+            WriteUint32(PRCType.PRC_TYPE_TESS_3D);
+            SerializeContent3DTess(t);
+        }
+
+        public void SerializeContentTessFace(PRCTessFace t)
+        {
+            WriteUint32(PRCType.PRC_TYPE_TESS_Face);
+            WriteUInt32List(t.line_attributes);
+            WriteUint32(t.start_wire);
+            WriteUInt32List(t.sizes_wire);
+            WriteUint32(t.used_entities_flag);
+            WriteUint32(t.start_triangulated);
+            WriteUInt32List(t.sizes_triangulated);
+
+            WriteUint32(t.number_of_texture_coordinate_indexes);
+
+            WriteBool(false); //HasVertexColors = False
+            if (t.line_attributes.Count > 0)
+                WriteUint32(t.behaviour);
+        }
+
+        protected void SerializeContentPRCBase(PRCContentBase t)
+        {
+            WriteUint32((UInt32)0); //Attributes
+            WriteString(t.name);
+            WriteUint32(t.CAD_identifier);
+            WriteUint32(t.CAD_persistent_identifier);
+            WriteUint32(t.PRC_unique_identifier);
+        }
+
+        public void SerializeContentMaterial(PRCMaterial t)
+        {
+            WriteUint32(PRCType.PRC_TYPE_GRAPH_Material);
+
+            SerializeContentPRCBase(t);
+
+            WriteUint32(t.ambient + 1);
+            WriteUint32(t.diffuse + 1);
+            WriteUint32(t.emissive + 1);
+            WriteUint32(t.specular + 1);
+            WriteDouble(t.shininess);
+            WriteDouble(t.ambient_alpha);
+            WriteDouble(t.diffuse_alpha);
+            WriteDouble(t.emissive_alpha);
+            WriteDouble(t.specular_alpha);
+        }
+
+        private void SerializeGraphics(PRCPolyBrepModel outStream)
+        {
+            WriteBool(false);
+        }
+
+        private void SerializePRCBaseWithGraphics(PRCPolyBrepModel t)
+        {
+            SerializeContentPRCBase(t);
+            SerializeGraphics(t);
+        }
+
+        private void SerializeRepresentationItemContent(PRCPolyBrepModel t)
+        {
+            SerializePRCBaseWithGraphics(t);
+            WriteUint32(t.index_local_coordinate_system + 1);
+            WriteUint32(t.index_tessellation + 1);
+        }
+
+        private void SerializeUserData<T>(T t)
+        {
+            WriteUint32(0); //No UserData
+        }
+
+        public void SerializePolyBrepModel(PRCPolyBrepModel t)
+        {
+            WriteUint32(PRCType.PRC_TYPE_RI_PolyBrepModel);
+            SerializeRepresentationItemContent(t);
+            WriteBool(t.is_closed);
+            SerializeUserData(t);
+        }
+
+        public void SerializeFileStructure(PRCFileStructure t)
+        {
+            t.h
+        }
+
+        public void SerializeFileStructureInformation(PRCFileStructure t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SerializeStartHeader(PRCStartHeader t)
+        {
+            WriteUncompressedBlock("PRC", 3);
+            WriteUncompressedUnsignedInteger(t.minimal_version_for_read);
+            WriteUncompressedUnsignedInteger(t.authoring_version);
+            SerializeFileStructureUncompressedUniqueId(t.file_structure_uuid);
+            SerializeFileStructureUncompressedUniqueId(t.application_uuid);
+        }
+
+        public void SerializePRCHeader(PRCHeader t)
+        {
+            SerializeStartHeader(t);
+
+            WriteUint32((UInt32)t.file_structure_information.Count);
+            foreach (var fileStructure in t.file_structure_information)
+                SerializeFileStructureInformation(fileStructure);
+
+            WriteUint32(t.model_file_offset);
+            WriteUint32(t.file_size);
+            WriteUint32(0); //No UncompressedFiles
+        }
+
+        public void SerializeSheme(PRCFile f)
+        {
+            WriteUint32(0); //No Shema
+        }
+
+        public void SerializeUnit(PRCUnit u)
+        {
+            WriteBool(u.unit_from_CAD_file);
+            WriteDouble(u.unit);
+        }
+
+        public void SerializeModelFileData(PRCFile f)
+        {
+            WriteUint32(PRCType.PRC_TYPE_ASM_ModelFile);
+            SerializeContentPRCBase(f);
+            SerializeUnit(f.unit);
+            WriteUint32(0); //product_occurences
+            WriteUint32(0); //file_structure_index_in_model_file
+            SerializeUserData(f);
+        }
+
+        public void SerializePRC(PRCFile f)
+        {         
+            f._header.file_structure_information.Clear();
+            f._header.file_structure_information.AddRange(f._fileStructures);
+            f._header.file_size = f.GetSize();
+            f._header.model_file_offset = f._header.file_size - 
+
+            SerializePRCHeader(f._header);
+
+            foreach (var fileStructure in f._fileStructures)
+                SerializeFileStructure(fileStructure);
+
+            SerializeSheme(f);
+            SerializeModelFileData(f);
         }
     }
 
@@ -97,59 +277,35 @@ namespace SharpPRC
 
         public const UInt32 PRC_TYPE_RI_PolyBrepModel = (PRC_TYPE_RI + 7);
 
+        public const UInt32 PRC_TYPE_ASM = 300;
+        public const UInt32 PRC_TYPE_ASM_ModelFile = (PRC_TYPE_ASM + 1);
+
         public const UInt32 PRC_TYPE_GRAPH = (700);
         public const UInt32 PRC_TYPE_GRAPH_Material = (PRC_TYPE_GRAPH + 2);
 
     }
 
-    public class PRC3DTess
+    public class PRCContentBaseTessData
     {
         internal bool is_calculated = false;
         internal readonly List<double> coordinates = new List<double>();
+    }
 
+    public class PRC3DTess : PRCContentBaseTessData
+    {
         internal bool has_faces = false;
         internal bool has_loops = false;
-
         internal readonly List<double> normal_coordinate = new List<double>();
         internal readonly List<UInt32> wire_index = new List<UInt32>();
         internal readonly List<UInt32> triangulated_index = new List<UInt32>();
-
         internal readonly List<PRCTessFace> face_tessellation = new List<PRCTessFace>();
-
         internal readonly List<double> texture_coordinate = new List<double>();
-
-        private void SerializeContentBaseTessData(PRCbitStreamWriter outStream)
-        {
-            outStream.Write(is_calculated);
-            outStream.Write(coordinates);
-        }
-
-        private void SerializeContent3DTess(PRCbitStreamWriter outStream)
-        {
-            SerializeContentBaseTessData(outStream);
-
-            outStream.Write(has_faces);
-            outStream.Write(has_loops);
-            outStream.Write(false); //must recalculate normals
-            outStream.Write(normal_coordinate);
-            outStream.Write(wire_index);
-            outStream.Write(triangulated_index);
-            outStream.Write(face_tessellation);
-            outStream.Write(texture_coordinate);
-        }
-
-        public void Serialize3DTess(PRCbitStreamWriter outStream)
-        {
-            outStream.Write(PRCType.PRC_TYPE_TESS_3D);
-            SerializeContent3DTess(outStream);
-        }
     }
 
     public class PRCTessFace
     {
         internal const UInt32 PRC_GRAPHICS_Show = 0x0001;
         internal const UInt32 PRC_FACETESSDATA_Triangle = 0x0002;
-
         internal readonly List<UInt32> line_attributes = new List<UInt32>();
         internal UInt32 start_wire = 0;
         internal readonly List<UInt32> sizes_wire = new List<UInt32>();
@@ -157,25 +313,7 @@ namespace SharpPRC
         internal UInt32 start_triangulated = 0;
         internal readonly List<UInt32> sizes_triangulated = new List<UInt32>();
         internal UInt32 number_of_texture_coordinate_indexes = 0;
-
         internal UInt32 behaviour = PRC_GRAPHICS_Show;
-
-        public void SerializeContentTessFace(PRCbitStreamWriter outStream)
-        {
-            outStream.Write(PRCType.PRC_TYPE_TESS_Face);
-            outStream.Write(line_attributes);
-            outStream.Write(start_wire);
-            outStream.Write(sizes_wire);
-            outStream.Write(used_entities_flag);
-            outStream.Write(start_triangulated);
-            outStream.Write(sizes_triangulated);
-
-            outStream.Write(number_of_texture_coordinate_indexes);
-
-            outStream.Write(false); //HasVertexColors = False
-            if (line_attributes.Count > 0)
-                outStream.Write(behaviour);
-        }
     }
 
     public class PRCContentBase
@@ -187,16 +325,6 @@ namespace SharpPRC
         internal readonly UInt32 CAD_identifier = cadID++;
         internal UInt32 CAD_persistent_identifier = 0;
         internal readonly UInt32 PRC_unique_identifier = prcID++;
-
-        protected void SerializeContentPRCBase(PRCbitStreamWriter outStream)
-        {
-            outStream.Write((UInt32)0); //Attributes
-
-            outStream.Write(name);
-            outStream.Write(CAD_identifier);
-            outStream.Write(CAD_persistent_identifier);
-            outStream.Write(PRC_unique_identifier);
-        }
     }
 
     public class PRCMaterial : PRCContentBase
@@ -211,62 +339,13 @@ namespace SharpPRC
         internal double diffuse_alpha;
         internal double emissive_alpha;
         internal double specular_alpha;
-
-        public void SerializeContentMaterial(PRCbitStreamWriter outStream)
-        {
-            outStream.Write(PRCType.PRC_TYPE_GRAPH_Material);
-
-            base.SerializeContentPRCBase(outStream);
-
-            outStream.Write(ambient + 1);
-            outStream.Write(diffuse + 1);
-            outStream.Write(emissive + 1);
-            outStream.Write(specular + 1);
-            outStream.Write(shininess);
-            outStream.Write(ambient_alpha);
-            outStream.Write(diffuse_alpha);
-            outStream.Write(emissive_alpha);
-            outStream.Write(specular_alpha);
-        }
     }
 
     public class PRCPolyBrepModel : PRCContentBase
     {
         internal bool is_closed = false;
-
         internal UInt32 index_local_coordinate_system = UInt32.MaxValue;
         internal UInt32 index_tessellation = UInt32.MaxValue;
-
-        private void SerializeGraphics(PRCbitStreamWriter outStream)
-        {
-            outStream.Write(false);
-        }
-
-        private void SerializePRCBaseWithGraphics(PRCbitStreamWriter outStream)
-        {
-            SerializeContentPRCBase(outStream);
-            SerializeGraphics(outStream);
-        }
-
-        private void SerializeRepresentationItemContent(PRCbitStreamWriter outStream)
-        {
-            SerializePRCBaseWithGraphics(outStream);
-            outStream.Write(index_local_coordinate_system + 1);
-            outStream.Write(index_tessellation + 1);
-        }
-
-        private void SerializeUserData(PRCbitStreamWriter outStream)
-        {
-            outStream.Write(0); //No UserData
-        }
-
-        public void SerializePolyBrepModel(PRCbitStreamWriter outStream)
-        {
-            outStream.Write(PRCType.PRC_TYPE_RI_PolyBrepModel);
-            SerializeRepresentationItemContent(outStream);
-            outStream.Write(is_closed);
-            SerializeUserData(outStream);
-        }
     }
 
     public class PRCTransform
@@ -274,13 +353,12 @@ namespace SharpPRC
         public readonly double[,] mat = new double[4, 4];
     }
 
-    public class PRCGroup
+    public class PRCFileStructure
     {
         internal readonly List<PRCPolyBrepModel> polymodels = new List<PRCPolyBrepModel>();
-
-        public void Serialize(PRCbitStreamWriter outStream)
+        internal uint GetStructureInformationSize()
         {
-            //Todo
+            throw new NotImplementedException();
         }
     }
 
@@ -291,12 +369,77 @@ namespace SharpPRC
         public double B;
     }
 
-    public class PRCFile
+    struct PRCUniqueId
     {
-        private readonly List<PRCTransform> _transforms = new List<PRCTransform>();
-        private readonly List<PRCRgbColor> _colors = new List<PRCRgbColor>();
-        private readonly List<PRCMaterial> _materials = new List<PRCMaterial>();
-        private readonly List<PRC3DTess> _tessellations = new List<PRC3DTess>();
+        static UInt32 count = 0;
+        UInt32 id0 = 0;
+        UInt32 id1 = 0;
+        UInt32 id2 = 0;
+        UInt32 id3 = 0;
+
+        public static PRCUniqueId MakeFileUUID()
+        {
+            return new PRCUniqueId()
+            {
+                id0 = 0x33595341,
+                id1 = (UInt32)DateTime.Now.Ticks,
+                id2 = count++,
+                id3 = (UInt32)new Random().Next(),
+            };
+        }
+    }
+
+    class PRCStartHeader
+    {
+        internal UInt32 minimal_version_for_read = 8137; // PRCVersion
+        internal UInt32 authoring_version = 8137;    // PRCVersion
+        internal PRCUniqueId file_structure_uuid = PRCUniqueId.MakeFileUUID();
+        internal PRCUniqueId application_uuid = new PRCUniqueId(); // should be 0
+
+        protected UInt32 GetStartHeaderSize()
+        {
+            return 3 + (2 + 2 * 4) * sizeof(UInt32);
+        }
+    };
+
+    class PRCHeader : PRCStartHeader
+    {
+        internal readonly List<PRCFileStructure> file_structure_information = new List<PRCFileStructure>();
+        internal UInt32 model_file_offset;
+        internal UInt32 file_size; // ??? 
+
+        public UInt32 getSize()
+        {
+            UInt32 size = GetStartHeaderSize() + sizeof(UInt32);
+            foreach (var fileStructure in file_structure_information)
+                size += fileStructure.GetStructureInformationSize();
+
+            size += 3 * sizeof(UInt32);
+
+            //foreach (var uncompressedFile in uncompressed_files)
+            //    size += uncompressedFile.GetSize();
+
+            return size;
+        }
+    }
+
+    class PRCUnit
+    {
+        public bool unit_from_CAD_file;
+        public double unit;
+    }
+
+    public class PRCFile : PRCContentBase
+    {
+        internal readonly PRCHeader _header = new PRCHeader();
+
+        internal readonly List<PRCTransform> _transforms = new List<PRCTransform>();
+        internal readonly List<PRCRgbColor> _colors = new List<PRCRgbColor>();
+        internal readonly List<PRCMaterial> _materials = new List<PRCMaterial>();
+        internal readonly List<PRC3DTess> _tessellations = new List<PRC3DTess>();
+
+        internal readonly List<PRCFileStructure> _fileStructures = new List<PRCFileStructure>();
+        internal PRCUnit unit;
 
         private UInt32 addColor(PRCRgbColor color)
         {
@@ -323,31 +466,6 @@ namespace SharpPRC
 
             _materials.Add(material);
             return (UInt32)_materials.Count - 1;
-        }
-
-        public void BeginGroup(string name, double[] transform = null)
-        {
-            //Todo
-        }
-
-        public void UseMesh(UInt32 tessIndex, UInt32 materialIndex)
-        {
-            //Todo
-        }
-
-        public void EndGroup()
-        {
-            //Todo
-        }
-
-        public void SerializePRCHeader(PRCbitStreamWriter outStream)
-        {
-            //Todo
-        }
-
-        public void SerializePRC(PRCbitStreamWriter outStream)
-        {
-            SerializePRCHeader(outStream);
         }
 
         public UInt32 createTriangleMesh(
@@ -396,6 +514,11 @@ namespace SharpPRC
             _tessellations.Add(tess);
 
             return (UInt32)(_tessellations.Count - 1);
+        }
+
+        internal uint GetSize()
+        {
+            throw new NotImplementedException();
         }
     }
 }
